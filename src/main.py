4a4533d825222fa38abd81a056f4c3e84ed5b697
JoshuaKellman <joshua@twoals.com>
2024-11-07 16:46:57 -0500

    first commit

---
+++ src/main.py
@@ -0,0 +1,33 @@
+
+from vex import *
+
+
+brain=Brain()
+
+controller=Controller()
+
+
+motor = Motor(Ports.PORT5)
+
+
+
+
+def on_up_button_pressed():
+    motor.spin(direction=REVERSE)
+
+def on_down_button_pressed():
+    motor.spin(direction=FORWARD)
+    
+
+
+def on_button_released():
+    motor.stop()
+
+
+controller.buttonR1.pressed(on_up_button_pressed)
+controller.buttonR1.released(on_button_released)
+controller.buttonR2.pressed(on_down_button_pressed)
+controller.buttonR2.released(on_button_released)
+
+
+        
