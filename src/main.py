e49f4f12befc384c959ee61bd3443ce87eea01a6
JoshuaKellman <joshua@twoals.com>
2024-11-07 21:14:06 -0500

    Added in drivetrain code. **This is not tested, no idea if it will work yet, this is a WIP**

--- src/main.py
+++ src/main.py
@@ -4,24 +4,24 @@ from vex import *
 
 brain=Brain()
 
-controller=Controller()
+controller=Controller(PRIMARY)
 
 
-motor = Motor(Ports.PORT5)
+lift = Motor(Ports.PORT5)
 
-motor.set_stopping(BrakeType.HOLD)
+lift.set_stopping(BrakeType.HOLD)
 
 
 def on_up_button_pressed():
-    motor.spin(direction=REVERSE)
+    lift.spin(direction=REVERSE)
 
 def on_down_button_pressed():
-    motor.spin(direction=FORWARD)
+    lift.spin(direction=FORWARD)
     
 
 
 def on_button_released():
-    motor.stop()
+    lift.stop()
 
 
 controller.buttonR1.pressed(on_up_button_pressed)
@@ -29,5 +29,66 @@ controller.buttonR1.released(on_button_released)
 controller.buttonR2.pressed(on_down_button_pressed)
 controller.buttonR2.released(on_button_released)
 
+controller_1_left_shoulder_control_motors_stopped = True
+controller_1_x_b_buttons_control_motors_stopped = True
+drivetrain_l_needs_to_be_stopped_controller_1 = False
+drivetrain_r_needs_to_be_stopped_controller_1 = False
+
+# define a task that will handle monitoring inputs from controller_1
+def rc_auto_loop_function_controller_1():
+    global drivetrain_l_needs_to_be_stopped_controller_1, drivetrain_r_needs_to_be_stopped_controller_1, controller_1_left_shoulder_control_motors_stopped, controller_1_x_b_buttons_control_motors_stopped, remote_control_code_enabled
+    # process the controller input every 20 milliseconds
+    # update the motors based on the input values
+    while True:
+        if remote_control_code_enabled:
+            
+            # calculate the drivetrain motor velocities from the controller joystick axies
+            # left = axis3 + axis1
+            # right = axis3 - axis1
+            drivetrain_left_side_speed = controller_1.axis3.position()
+            drivetrain_right_side_speed = controller_1.axis2.position()
+            
+            # check if the value is inside of the deadband range
+            if drivetrain_left_side_speed < 5 and drivetrain_left_side_speed > -5:
+                # check if the left motor has already been stopped
+                if drivetrain_l_needs_to_be_stopped_controller_1:
+                    # stop the left drive motor
+                    left_drive_smart.stop()
+                    # tell the code that the left motor has been stopped
+                    drivetrain_l_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the left motor next
+                # time the input is in the deadband range
+                drivetrain_l_needs_to_be_stopped_controller_1 = True
+            # check if the value is inside of the deadband range
+            if drivetrain_right_side_speed < 5 and drivetrain_right_side_speed > -5:
+                # check if the right motor has already been stopped
+                if drivetrain_r_needs_to_be_stopped_controller_1:
+                    # stop the right drive motor
+                    right_drive_smart.stop()
+                    # tell the code that the right motor has been stopped
+                    drivetrain_r_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the right motor next
+                # time the input is in the deadband range
+                drivetrain_r_needs_to_be_stopped_controller_1 = True
+            
+            # only tell the left drive motor to spin if the values are not in the deadband range
+            if drivetrain_l_needs_to_be_stopped_controller_1:
+                left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
+                left_drive_smart.spin(FORWARD)
+            # only tell the right drive motor to spin if the values are not in the deadband range
+            if drivetrain_r_needs_to_be_stopped_controller_1:
+                right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
+                right_drive_smart.spin(FORWARD)
+            # check the buttonL1/buttonL2 status
+            # to control scooper_motor
+        # wait before repeating the process
+        wait(20, MSEC)
+
+# define variable for remote controller enable/disable
+remote_control_code_enabled = True
+
+rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
 
         
