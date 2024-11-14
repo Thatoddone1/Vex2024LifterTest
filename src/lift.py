ecbb777a4350efa6e2844b916fb2f7b86550aeee
JoshuaKellman <joshua@twoals.com>
2024-11-14 16:38:23 -0500

    split the drive and lifter code into serperate files. might move stuff around later

---
+++ src/lift.py
@@ -0,0 +1,26 @@
+from vex import *
+from main import controller
+
+lift = Motor(Ports.PORT5)
+
+lift.set_stopping(BrakeType.HOLD)
+
+
+def on_up_button_pressed():
+    lift.spin(direction=REVERSE)
+
+def on_down_button_pressed():
+    lift.spin(direction=FORWARD)
+    
+
+
+def on_button_released():
+    lift.stop()
+
+
+
+# controller event handlers
+controller.buttonR1.pressed(on_up_button_pressed)
+controller.buttonR1.released(on_button_released)
+controller.buttonR2.pressed(on_down_button_pressed)
+controller.buttonR2.released(on_button_released)
