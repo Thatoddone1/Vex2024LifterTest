2235a92a20debc7d57043d3141cfd1ece939bd08
JoshuaKellman <joshua@twoals.com>
2024-11-26 16:31:30 -0500

    Removed extra whitespace. Renamed `controller_1` varibles to `controller`

--- src/main.py
+++ src/main.py
@@ -3,13 +3,7 @@ from vex import *
 #CONFIG
 
 brain=Brain()
-
 controller=Controller(PRIMARY)
-
-
-
-
-
 
 ####### LIFT STUFF ↓ #######    
 
@@ -17,26 +11,20 @@ lift = Motor(Ports.PORT5)
 
 lift.set_stopping(BrakeType.HOLD)
 
-
 def on_up_button_pressed():
     lift.spin(direction=REVERSE)
 
 def on_down_button_pressed():
     lift.spin(direction=FORWARD)
     
-
-
 def on_lift_button_released():
     lift.stop()
-
-
 
 # controller event handlers
 controller.buttonR1.pressed(on_up_button_pressed)
 controller.buttonR1.released(on_lift_button_released)
 controller.buttonR2.pressed(on_down_button_pressed)
 controller.buttonR2.released(on_lift_button_released)
-
 
 ####### CLAW CODE ↓ ########
 
@@ -51,12 +39,8 @@ def on_close_button_pressed():
 def on_open_button_pressed():
     claw.spin(direction=FORWARD)
     
-
-
 def on_claw_button_released():
     claw.stop()
-
-
 
 # controller event handlers
 controller.buttonL1.pressed(on_open_button_pressed)
@@ -64,12 +48,7 @@ controller.buttonL1.released(on_claw_button_released)
 controller.buttonL2.pressed(on_close_button_pressed)
 controller.buttonL2.released(on_claw_button_released)
 
-
-
 ####### DRIVE STUFF ↓ ########
-
-
-
 # motors and drive train setup
 left_motor_a = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
 left_motor_b = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
@@ -80,14 +59,12 @@ right_drive_smart = MotorGroup(right_motor_a, right_motor_b)
 drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 355.59999999999997, 266.7, MM, 1)
 
 #varibles needed for the controoler loop
-drivetrain_l_needs_to_be_stopped_controller_1 = False
-drivetrain_r_needs_to_be_stopped_controller_1 = False
+drivetrain_l_needs_to_be_stopped_controller = False
+drivetrain_r_needs_to_be_stopped_controller = False
 
-
-
-# define a task that will handle monitoring inputs from controller_1
-def rc_auto_loop_function_controller_1():
-    global drivetrain_l_needs_to_be_stopped_controller_1, drivetrain_r_needs_to_be_stopped_controller_1, controller_1_left_shoulder_control_motors_stopped, controller_1_x_b_buttons_control_motors_stopped, remote_control_code_enabled
+# define a task that will handle monitoring inputs from controller
+def rc_auto_loop_function_controller():
+    global drivetrain_l_needs_to_be_stopped_controller, drivetrain_r_needs_to_be_stopped_controller, controller_left_shoulder_control_motors_stopped, controller_x_b_buttons_control_motors_stopped, remote_control_code_enabled
     # process the controller input every 20 milliseconds
     # update the motors based on the input values
     while True:
@@ -102,34 +79,34 @@ def rc_auto_loop_function_controller_1():
             # check if the value is inside of the deadband range
             if drivetrain_left_side_speed < 5 and drivetrain_left_side_speed > -5:
                 # check if the left motor has already been stopped
-                if drivetrain_l_needs_to_be_stopped_controller_1:
+                if drivetrain_l_needs_to_be_stopped_controller:
                     # stop the left drive motor
                     left_drive_smart.stop()
                     # tell the code that the left motor has been stopped
-                    drivetrain_l_needs_to_be_stopped_controller_1 = False
+                    drivetrain_l_needs_to_be_stopped_controller = False
             else:
                 # reset the toggle so that the deadband code knows to stop the left motor next
                 # time the input is in the deadband range
-                drivetrain_l_needs_to_be_stopped_controller_1 = True
+                drivetrain_l_needs_to_be_stopped_controller = True
             # check if the value is inside of the deadband range
             if drivetrain_right_side_speed < 5 and drivetrain_right_side_speed > -5:
                 # check if the right motor has already been stopped
-                if drivetrain_r_needs_to_be_stopped_controller_1:
+                if drivetrain_r_needs_to_be_stopped_controller:
                     # stop the right drive motor
                     right_drive_smart.stop()
                     # tell the code that the right motor has been stopped
-                    drivetrain_r_needs_to_be_stopped_controller_1 = False
+                    drivetrain_r_needs_to_be_stopped_controller = False
             else:
                 # reset the toggle so that the deadband code knows to stop the right motor next
                 # time the input is in the deadband range
-                drivetrain_r_needs_to_be_stopped_controller_1 = True
+                drivetrain_r_needs_to_be_stopped_controller = True
             
             # only tell the left drive motor to spin if the values are not in the deadband range
-            if drivetrain_l_needs_to_be_stopped_controller_1:
+            if drivetrain_l_needs_to_be_stopped_controller:
                 left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
                 left_drive_smart.spin(FORWARD)
             # only tell the right drive motor to spin if the values are not in the deadband range
-            if drivetrain_r_needs_to_be_stopped_controller_1:
+            if drivetrain_r_needs_to_be_stopped_controller:
                 right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
                 right_drive_smart.spin(FORWARD)
             # check the buttonL1/buttonL2 status
@@ -137,9 +114,7 @@ def rc_auto_loop_function_controller_1():
         # wait before repeating the process
         wait(20, MSEC)
 
-
-
 # define variable for remote controller enable/disable
 remote_control_code_enabled = True
 
-rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
+rc_auto_loop_thread_controller = Thread(rc_auto_loop_function_controller)
